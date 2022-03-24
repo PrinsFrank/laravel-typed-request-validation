944576a9e67f13748ba7e711cc4bf448d6b88196
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-03-24 20:14:23 +0100

    Add serviceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\LaravelTypedRequestValidation;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+}
